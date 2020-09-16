597e847d64fd2b83600b1e7ed1370616e6f5ec9b
Shobhit Yadav <shobhit999000@gmail.com>
2020-09-16 23:59:29 +0530

    Add files via upload

---
+++ Expressions and Operators/Swaptwovariableswithoutusingathirdvariable.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* Include other headers as needed */
+int main()
+{
+  int a,b;
+  scanf("%d\n",&a);
+  scanf("%d\n",&b);
+  printf("%d\n",b);
+  printf("%d\n",a);
+    return 0;
+}
