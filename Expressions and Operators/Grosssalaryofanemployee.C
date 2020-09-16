65b922439f1938dd6edcf043d442ee80784e9807
Shobhit Yadav <shobhit999000@gmail.com>
2020-09-16 23:58:08 +0530

    Add files via upload

---
+++ Expressions and Operators/Grosssalaryofanemployee.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* Include other headers as needed */
+int main()
+{
+  int a,b,c,d;
+  scanf("%d",&a);
+  b=a*.1;
+  c=a*.2;
+  d=a+b+c;
+  printf("%d",d);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
