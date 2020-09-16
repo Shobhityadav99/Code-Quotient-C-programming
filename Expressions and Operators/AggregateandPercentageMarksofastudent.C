2f056c33d823f245b952e301077d1ae26b2ac093
Shobhit Yadav <shobhit999000@gmail.com>
2020-09-16 23:56:54 +0530

    Add files via upload

---
+++ Expressions and Operators/AggregateandPercentageMarksofastudent.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* Include other headers as needed */
+int main()
+{
+int a,b,c,d,e,f,g;
+  scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+  f=a+b+c+d+e;
+  g=f/5;
+  printf("%d %d",f,g);
+    return 0;
+}
