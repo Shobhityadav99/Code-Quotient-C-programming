0a9fe08af20a917dc3e9cebac5285a7a2221ad19
Shobhit Yadav <shobhit999000@gmail.com>
2020-09-18 01:53:30 +0530

    Add files via upload

---
+++ Practice Questions-I/Forma4digitnumber.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* Include other headers as needed */
+int main()
+{
+  int a,b,c,d;
+  scanf("%d %d %d %d",&a,&b,&c,&d);
+  printf("%d%d%d%d",a,b,c,d);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
