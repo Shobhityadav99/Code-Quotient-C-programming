907138914bbb62d6fe4d53a2b9bac88783a3b243
Shobhit Yadav <shobhit999000@gmail.com>
2020-09-18 01:55:10 +0530

    Add files via upload

---
+++ Practice Questions-I/Sumofthefirstandthelastdigitofa4digitnumber.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* Include other headers as needed */
+int main()
+{
+   int a;
+  scanf("%d",&a);
+  int b=a/1000;
+  int c=a%10;
+  int d=b+c;
+  printf("%d",d);
+    return 0;
+}
