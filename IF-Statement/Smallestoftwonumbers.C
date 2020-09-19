8282ac8402cecd563f19b489269e29723311470c
Shobhit Yadav <shobhit999000@gmail.com>
2020-09-20 00:40:00 +0530

    Add files via upload

---
+++ IF-Statement/Smallestoftwonumbers.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* Include other headers as needed */
+int main()
+{
+  int a,b;
+  scanf("%d %d",&a,&b);
+  if(a<b)
+  {
+    printf("%d",a);
+  }
+  else
+    printf("%d",b);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
