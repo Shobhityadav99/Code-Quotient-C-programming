187c2b035534df31b2e7a9f3a0e08a9060f8b6b8
Shobhit Yadav <shobhit999000@gmail.com>
2020-09-19 13:54:08 +0530

    Add files via upload

---
+++ IF-Statement/Evenandodd.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* Include other headers as needed */
+int main()
+{
+int a;
+  scanf("%d",&a);
+  if(a%2==0)
+    printf("Even");
+  else
+    printf("Odd");
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
