4fb31f5129ce2f3ccac9f6cc926b5931c7d5c76a
Shobhit Yadav <shobhit999000@gmail.com>
2020-09-19 13:42:27 +0530

    Add files via upload

---
+++ IF-Statement/Equalornot.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* Include other headers as needed */
+int main()
+{
+  int a,b;
+  scanf("%d %d",&a,&b);
+  if(a==b)
+    printf("Equal");
+  else
+    printf("Not Equal");
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
