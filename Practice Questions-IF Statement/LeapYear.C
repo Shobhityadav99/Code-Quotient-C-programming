82c2526f56afc49427b9fbf0c9e3a8d32363a6f7
Shobhit Yadav <shobhit999000@gmail.com>
2020-09-21 01:25:23 +0530

    Add files via upload

---
+++ Practice Questions-IF Statement/LeapYear.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* Include other headers as needed */
+int main()
+{
+  int a;
+  scanf("%d",&a);
+  if(((a%100!=0)&&(a%4==0))||(a%400==0))
+  {
+    printf("Leap Year");
+  }
+  else
+  {
+    printf("Not a Leap Year");
+  }
+    return 0;
+}
