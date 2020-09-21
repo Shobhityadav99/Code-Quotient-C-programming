37b35b2c700a1503e2209cf7b41842fabbd9006f
Shobhit Yadav <shobhit999000@gmail.com>
2020-09-22 03:35:57 +0530

    Add files via upload

---
+++ Conditional Operators and Switch Statement/Givencharacterisvowelconsonants.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+   char a;
+  scanf("%c",&a);
+  switch(a)
+  {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    case 'A':
+    case 'E':
+    case 'I':
+    case 'O':
+    case 'U':
+      printf("vowel");
+      break;
+    default:
+      printf("consonant");
+      break;
+      
+      
+  }
+    return 0;
+}
