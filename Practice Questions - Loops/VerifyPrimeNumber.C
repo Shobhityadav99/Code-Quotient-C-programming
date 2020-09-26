5a51217d768e85c545fccb8ed91155314d121641
Shobhit Yadav <shobhit999000@gmail.com>
2020-09-27 02:34:59 +0530

    Add files via upload

---
+++ Practice Questions - Loops/VerifyPrimeNumber.C
@@ -0,0 +1,15 @@
+int verifyPrime(int n)
+{
+  int c;
+  if (n==0 || n==1)
+  {
+    return 0;
+  }
+ else
+   for ( c = 2 ; c <= n - 1 ; c++ )
+   { 
+      if ( n%c == 0 )
+     return 0;
+   }
+   return 1;
+}
