819f89d9e89830d8e49ce2d9448cdb1de864673f
Shobhit Yadav <shobhit999000@gmail.com>
2020-09-27 02:33:40 +0530

    Add files via upload

---
+++ Practice Questions - Loops/Primefactorsofanumber.C
@@ -0,0 +1,19 @@
+#include <math.h>
+void primeFactors(int n) 
+{
+    while (n%2 == 0) 
+    { 
+        printf("%d\n", 2); 
+        n = n/2; 
+    }
+    for (int i = 3; i <= sqrt(n); i = i+2) 
+    {
+        while (n%i == 0) 
+        { 
+            printf("%d\n", i); 
+            n = n/i; 
+        } 
+    } 
+    if (n > 2) 
+        printf ("%d\n", n); 
+} 
