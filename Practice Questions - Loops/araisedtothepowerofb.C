ca43a4d6f9789c1c57b56327c7d3b88e4c780509
Shobhit Yadav <shobhit999000@gmail.com>
2020-09-27 02:30:57 +0530

    Add files via upload

---
+++ Practice Questions - Loops/araisedtothepowerofb.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+/* Include other headers as needed */
+int main()
+{
+   int a,b;
+  scanf("%d\n",&a);
+  scanf("%d\n",&b);
+  int c=pow(a,b);
+  printf("%d",c);
+  
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
