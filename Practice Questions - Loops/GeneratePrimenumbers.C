98da85c1b50160f1d7171de8effde364488e867f
Shobhit Yadav <shobhit999000@gmail.com>
2020-09-27 02:32:41 +0530

    Add files via upload

---
+++ Practice Questions - Loops/GeneratePrimenumbers.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, end, isPrime; 
+    scanf("%d", &end);
+    for(i=2; i<=end; i++)
+    {
+        isPrime = 1; 
+        for(j=2; j<=i/2; j++)
+        {
+            if(i%j==0)
+            {
+                isPrime = 0;
+                break;
+            }
+        }
+        if(isPrime==1)
+                  {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
